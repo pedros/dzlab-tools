b9efb0988f82d62251692d9508ccc1ab07160bab
Pedro Silva <pedros@berkeley.edu>
2009-05-19 21:24:33 +0000

    * first commit

---
+++ gen_insilico/merge_scaffs.pl
@@ -0,0 +1,135 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use Data::Dumper;
+use Carp;
+use Getopt::Long;
+use Pod::Usage;
+
+# Check required command line parameters
+unless (@ARGV > 0) {
+    pod2usage ( -verbose => 1 );
+}
+
+my $scaffolds;
+my $max_length = 20000000;
+
+# Grabs and parses command line options
+my $result = GetOptions (
+    'scaffolds|s=s'  => \$scaffolds,
+    'max-length|l=i' => \$max_length,
+    'verbose|v' => sub { use diagnostics; },
+    'quiet|q'   => sub { no warnings; },
+    'help|h'    => sub { pod2usage ( -verbose => 1 ); },
+    'manual|m'  => sub { pod2usage ( -verbose => 2 ); }
+);
+
+my $current_length = 0;
+my @objects = ();
+
+my @reference = @{ index_fasta ($scaffolds) };
+
+for my $scaff (@reference) {
+
+    if ($current_length + length $scaff->[1] < $max_length) {
+        push @objects, $scaff;
+        $current_length += length $scaff->[1];
+    }
+    else {
+        print ">$objects[0]->[0]-$objects[$#objects]->[0]\n";
+
+        print $_->[1], "\n"
+        for @objects;
+
+        $current_length = 0;
+        @objects = ();
+
+        push @objects, $scaff;
+        $current_length += length $scaff->[1];
+    }
+}
+
+exit 0;
+
+
+# index_fasta reads a standard fasta file and returns a hash with chromosome names as keys as chromosome lengths as values
+sub index_fasta {
+    my $reference_file = shift;
+
+    # holds name of chromosomes as keys and length of chromosomes in bp as values
+    my @reference = ();
+
+    return \@reference unless $reference_file;
+
+    # reads in the reference genome file into @fastaseq
+    open my $REF, '<', "$reference_file" or croak "Can't open file: $reference_file";
+    my @fastaseq = <$REF>;
+    close $REF;
+
+    # find and store indices for each chromosome change and corresponding descriptions
+    my ( @idx, @dsc ) = ();
+    for my $i ( 0 .. @fastaseq - 1 ) {
+        if ( $fastaseq[$i] =~ m/^>/ ) {
+            $fastaseq[$i] =~ s/>//gi;
+            $fastaseq[$i] = ( split /\s/, "$fastaseq[$i]" )[0];
+            $fastaseq[$i] =~ tr/A-Z/a-z/;
+            push @idx, $i;
+            push @dsc, $fastaseq[$i];
+        }
+    }
+
+    # tries to find each chromosome's centrometer center coordinate
+    for my $j ( 0 .. @idx - 1 ) {
+        my $line;
+        if ( $j == scalar @idx - 1 ) {
+            $line = join( q{}, @fastaseq[ $idx[$j] + 1 .. @fastaseq - 1]);
+        }
+        else {
+            $line = join( q{}, @fastaseq[ $idx[$j] + 1 .. $idx[$j + 1] - 1]);
+        }
+        $line =~ s/[\n\r]//g;
+        push @reference, [$dsc[$j], $line];
+    }
+    return \@reference;
+}
+
+
+
+__END__
+
+
+=head1 NAME
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 OPTIONS
+
+=head1 REVISION
+
+=head1 AUTHOR
+
+ Pedro Silva <psilva@nature.berkeley.edu/>
+ Zilberman Lab <http://dzlab.pmb.berkeley.edu/>
+ Plant and Microbial Biology Department
+ College of Natural Resources
+ University of California, Berkeley
+
+=head1 COPYRIGHT
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+=cut
