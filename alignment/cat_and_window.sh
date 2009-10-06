0076cd4efa8509068dfcb895302164d4486f1917
Pedro Silva <pedros@berkeley.edu>
2009-10-06 21:55:33 +0000

    minor commit

--- alignment/cat_and_window.sh
+++ alignment/cat_and_window.sh
@@ -4,6 +4,10 @@ echo "Starting cat and window run at" `date`
 
 NAME=$1
 GENOME=$2
+GENES=$3
+TRANSPOSONS=$4
+shift
+shift
 shift
 shift
 BATCHES=$@
@@ -11,8 +15,6 @@ groups=`grep '>' $GENOME | perl -ne 's/>([^\s]+)//; print $1, "\n"' | tr "[:uppe
 width=50
 step=50
 contexts='CG CHG CHH'
-
-# GSM399598_WT_endosperm_BS_seq_w50-wt_endosperm_CHH
 
 echo "Concatenating single c files..."
 mkdir -p post-processing/single-c
@@ -22,24 +24,47 @@ for i in $groups; do
             # ls ${j}/single-c/*${i}*single-c*${k}*gff
             # ls post-processing/single-c/${NAME}_BS-Seq_${i}_${k}_w1_methylation.gff
             cat ${j}/single-c/*${i}*single-c*${k}*gff >> post-processing/single-c/${NAME}_BS-Seq_${i}_${k}_w1_methylation.gff
+            cat ${j}/single-c/*${i}*single-c.gff.freq >> post-processing/single-c/${NAME}_BS-Seq_${i}_methylation.freq
         done
     done
 done
 echo "Done with code: $?"
 
+# echo "split_gff.pl  --sequence all $GENES"
+# echo "split_gff.pl  --sequence all $TRANSPOSONS"
 
 echo "Merging and windowing concatenated single c files"
 mkdir -p post-processing/windows
+mkdir -p post-processing/ends-analysis/dat
 for i in $groups; do
     for j in $contexts; do
 
         # ls post-processing/single-c/${NAME}_BS-Seq_${i}_${k}_w1_methylation.gff
         # ls post-processing/windows/${NAME}_BS-Seq_${i}_${k}_w${width}_methylation.gff
-        window_gff.pl --gff-file post-processing/single-c/${NAME}_BS-Seq_${i}_${k}_w1_methylation.gff --width ${width} --step ${step} --no-skip --output post-processing/windows/${NAME}_BS-Seq_${i}_${k}_w${width}_methylation.gff
+        window_gff.pl --gff-file post-processing/single-c/${NAME}_BS-Seq_${i}_${j}_w1_methylation.gff --width ${width} --step ${step} --no-skip --output post-processing/windows/${NAME}_BS-Seq_${i}_${j}_w${width}_methylation.gff
+        
+        # for annotation in genes transposons; do
+        #     for origin in 3prime 5prime; do
+        #         for wflag in 2 '6-1500'; do
+
+        #             if [ "$annotation" = "genes" ]; then
+        #                 ANNOTATION=$GENES
+        #             else
+        #                 ANNOTATION=$TRANSPOSONS
+        #             fi
+        #             flag=`echo $wflag | sed 's/-/ /'`
+
+        #             echo "makeDist.pl `echo $ANNOTATION | sed 's/\.gff/-${i}.gff/'` post-processing/single-c/${NAME}_BS-Seq_${i}_${j}_w1_methylation.gff post-processing/ends-analysis/${NAME}_BS-Seq_${i}_${j}_w1_methylation_${annotation}_${origin}_${wflag} 100 5000 $flag $origin"
+        #             echo "average.pl post-processing/ends-analysis/${NAME}_BS-Seq_${i}_${j}_w1_methylation_${annotation}_${origin}_${flag} 100 100 > post-processing/ends-analysis/dat/${NAME}_BS-Seq_${i}_${j}_w1_methylation_${annotation}_${origin}_${wflag}.dat"
+        #         done
+        #     done
+        # done
 
     done
 done
 echo "Done with code: $?"
+
+
 
 
 echo "Finished cat and window run at" `date`
