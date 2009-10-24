84dee3ae235820c11c0929b47ca1b787923b52a3
Pedro Silva <pedros@berkeley.edu>
2009-10-24 02:09:13 +0000

    first commit

---
+++ alignment/ends_analysis.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+echo "Starting ends analysis run at" `date`
+
+NAME=$1
+GENOME=$2
+annotations=$3
+contexts=$4
+orientations=$5
+FLAG=$6
+OUTDIR=$7
+
+avoid=( groupmt mt chrmt chrc chrm dmel_mitochondrion_genome group_c519-c654 scaffold_m162 )
+groups=`grep '>' $GENOME | perl -ne 's/>([^\s]+)//; print $1, "\n"' | tr "[:upper:]" "[:lower:]"`
+
+cd $NAME
+#rm -r ${OUTDIR}
+echo "Computing scores"
+mkdir -p ${OUTDIR}/{log,dat}
+
+for i in $groups; do
+    
+    skipped=`perl -e '$a = shift; $avoid = join q{|}, @ARGV; if ($a =~ /($avoid)/) {print $1; exit 1} else {exit 0}' $i ${avoid[@]}`
+    result=$?
+
+    if [ $result -eq 0 ]; then
+
+        for j in $contexts; do
+            for k in $annotations; do
+                for l in $orientations; do
+
+                    atype=`echo $k | cut -d'|' -f1`
+                    annotation=`echo $k | cut -d'|' -f2 | sed s/\.gff/-$i.gff/`
+                    cat_flag=`echo $FLAG | sed 's/ /-/'`
+                    fin=${NAME}_BS-Seq_${i}_${j}_w1_methylation.gff
+                    fout=${NAME}_BS-Seq_${i}_${j}_${atype}_${l}_flag${cat_flag}
+
+                    # echo '==================='
+                    # echo $atype
+                    # echo $annotation
+                    # echo $fin
+                    # echo $fout
+                    # echo '==================='                   
+                    # if [ ! -e post-processing/single-c/$fin ]; then
+                    #     echo $fin "DOES NOT EXIST"
+                    # fi
+
+                    makeDist.pl $annotation post-processing/single-c/${fin} ${OUTDIR}/${fout} 100 5000 $FLAG $l
+                    mv ${OUTDIR}/${fout}.log ${OUTDIR}/log/${fout}.log
+                    average.pl ${OUTDIR}/${fout}.ends 100 100 > ${OUTDIR}/dat/${fout}.dat
+                    
+                done
+            done
+        done
+
+    fi
+    
+done
+
+
+for j in $contexts; do
+    for k in $annotations; do
+        for l in $orientations; do
+            
+            atype=`echo $k | cut -d'|' -f1`
+            cat_flag=`echo $FLAG | sed 's/ /-/'`
+
+            cat-dat.pl ${OUTDIR}/dat/${NAME}_BS_Seq_*_${j}_${atype}_${l}_flag${cat_flag}.dat
+            
+        done
+    done
+done
+
+
+echo "Finished ends analysis run at" `date`
