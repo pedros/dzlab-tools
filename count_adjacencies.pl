18efcca58ade58040322a9770c3fa071e178a208
Pedro Silva <pedros@berkeley.edu>
2010-01-10 10:03:20 +0000

    First commit

---
+++ count_adjacencies.pl
@@ -0,0 +1,284 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use Data::Dumper;
+use Carp;
+use Getopt::Long;
+use Pod::Usage;
+use List::Util qw/sum/;
+
+my $GFF_DATA  = 'ARGV';
+my $min_score = 0.10;
+my $min_total = 10;
+my $min_gap   = 1;
+my $normalize = q{};
+my $sort      = 0;
+my $output;
+
+# Grabs and parses command line options
+my $result = GetOptions (
+    'min-score|s=f' => \$min_score,
+    'min-total|t=i' => \$min_total,
+    'min-gap|g=i'   => \$min_gap,
+    'normalize|n=s' => \$normalize,
+    'sort|r'        => \$sort,
+    'output|o=s'  => \$output,
+    'verbose|v'   => sub { use diagnostics; },
+    'quiet|q'     => sub { no warnings; },
+    'help|h'      => sub { pod2usage ( -verbose => 1 ); },
+    'manual|p'    => sub { pod2usage ( -verbose => 2 ); }
+);
+
+# Check required command line parameters
+pod2usage ( -verbose => 1 )
+unless @ARGV and $result and $min_score > 0;
+
+if ($output) {
+    open my $USER_OUT, '>', $output or croak "Can't open $output for writing: $!";
+    select $USER_OUT;
+}
+
+if ($sort) {
+    my $sorted_filename = sort_gff (@ARGV);
+    $GFF_DATA = undef;
+    open $GFF_DATA, '<', $sorted_filename;
+}
+
+my $reference = index_fasta ($normalize);
+
+my %buffer = ();
+my %counts = ();
+
+while (<$GFF_DATA>) {
+    next if m/^#.*$|^\s*$/;
+    chomp;
+    s/[\n\r]//;
+
+    my $current = gff_read ($_);
+    my $seqid   = $current->{seqname};
+    my $score   = $current->{score};
+    my $total   = sum map { (split /=/, $_)[1] } split /;/, $current->{attribute};
+
+    # initialize buffer
+    if (!exists $buffer{$seqid} or !@{$buffer{$seqid}}) {
+        push @{$buffer{$seqid}}, [$score, $total, $current->{end}]
+        if $score ne q{.} and $score > $min_score and $total > $min_total;
+    }
+    # extend buffer if possible
+    elsif ($current->{start} - $buffer{$seqid}->[-1]->[2] <= $min_gap
+           and $score ne q{.} and $score > $min_score and $total > $min_total) {
+        push @{$buffer{$seqid}}, [$score, $total, $current->{end}];
+    }
+    # flush buffer
+    else {
+        flush_buffer (\%buffer, \%counts, $seqid);
+    }
+}
+
+my %full_counts = ();
+
+# post-process histogram counts, one chromosome at a time
+for my $seqid (keys %counts) {
+
+    # flush whatever's left over, if anything
+    flush_buffer (\%buffer, \%counts, $seqid)
+    if @{$buffer{$seqid}};
+ 
+    # slide through each bin size in ascending sorted order
+    for my $size (sort { $a <=> $b } keys %{$counts{$seqid}}) {
+
+        # find number of hits per bin, mean score and deviations
+        my $number = scalar @{$counts{$seqid}{$size}{mean}};
+        # my $mean   = (sum @{$counts{$seqid}{$size}{mean}}) / $number;
+        # my $var    = (sum map { ($_ - $mean) ** 2 }
+        #               @{$counts{$seqid}{$size}{mean}}) / ($number - 1 || 1);
+        # my $std    = $var ** 1/2;
+    
+        # normalize number of hits with bulk size of genome
+        if ($normalize and $reference and exists $reference->{$seqid}) {
+            $number /= (length $reference->{$seqid});
+            $number  = sprintf ("%g", $number * 1000000);
+        }
+
+        # print join ("\t",
+        #             $size,
+        #             $number,
+        #             sprintf ("%g", $mean),
+        #             sprintf ("%g", $var),
+        #             sprintf ("%g", $std),
+        #         ), "\n";
+
+        $full_counts{$size} += $number;
+    }
+}
+
+#print "#$seqid, counts normalized per 1000bp\n";
+#print "#Window size\tWindow number\tMean score\tVariance\tStd\n";
+
+for my $size (sort {$a <=> $b } keys %full_counts) {
+    print $size, "\t", $full_counts{$size}, "\n";
+}
+
+
+sub flush_buffer {
+    # alters $buffer and $counts in place
+    my ($buffer, $counts, $seqid) = @_;
+
+    my $size = scalar @{$buffer->{$seqid}};
+    my $mean = (sum map {  $_->[0]               } @{$buffer->{$seqid}}) / $size || 1;
+    # my $var  = (sum map { ($_->[0] - $mean) ** 2 } @{$buffer->{$seqid}}) / ($size - 1 || 1);
+
+    push @{$counts->{$seqid}{$size}{mean}}, $mean;
+    # push @{$counts->{$seqid}{$size}{var }}, $var;
+    # push @{$counts->{$seqid}{$size}{std }}, $var ** 1/2;
+    
+    @{$buffer->{$seqid}} = ();
+}
+
+
+sub gff_read {
+    my ($seqname, $source, $feature, $start, $end, $score, $strand, $frame, $attribute)
+    = split /\t/, shift;
+
+    return {
+	'seqname'  => $seqname,
+	'source'   => $source,
+	'feature'  => $feature,
+	'start'    => $start,
+	'end'      => $end,
+	'score'    => $score,
+	'strand'   => $strand,
+	'frame'    => $frame,
+	'attribute'=> $attribute
+    };
+}
+
+
+sub sort_gff {
+    use File::Temp qw/tempfile/;
+
+    my @gff_files               = @_;
+    my @gff                     = ();
+    my ($tmp_fh, $tmp_filename) = tempfile();
+
+    BATCH:
+    for my $gff_file (@gff_files) {
+
+        open my $GFF, '<', $gff_file 
+        or croak "Can't read $gff_file: $!";
+
+        while (<$GFF>) {
+            next if ($_ =~ m/^#.*$|^\s*$/);
+            chomp;
+            push @gff, $_;
+        }
+
+        close $GFF
+        or carp "Can't close $gff_file: $!";
+    }
+
+    map  { print $tmp_fh $_, "\n" }
+    sort {
+        (split /\t/, $a)[0] cmp (split /\t/, $b)[0] or
+        (split /\t/, $a)[3] <=> (split /\t/, $b)[3]
+    }
+    @gff;
+
+    return $tmp_filename;
+}
+
+
+sub index_fasta {
+    my $reference_file = shift;
+
+    my %reference = ();
+
+    return \%reference unless $reference_file;
+
+    # reads in the reference genome file into @fastaseq
+    open my $REF, '<', "$reference_file" or croak "Can't open $reference_file for reading: $!";
+    my @fastaseq = <$REF>;
+    close $REF;
+
+    # find and store indices for each chromosome change and corresponding descriptions
+    my ( @idx, @dsc ) = ();
+    for my $i ( 0 .. @fastaseq - 1 ) {
+        if ( $fastaseq[$i] =~ m/^>/ ) {
+            $fastaseq[$i] =~ s/>//g;
+            $fastaseq[$i] = ( split /\s/, "$fastaseq[$i]" )[0];
+            $fastaseq[$i] =~ tr/A-Z/a-z/;
+            push @idx, $i;
+            push @dsc, $fastaseq[$i];
+        }
+    }
+
+    for my $j ( 0 .. @idx - 1 ) {
+        my $line;
+        if ( $j == scalar @idx - 1 ) {
+            $line = join( q{}, @fastaseq[ $idx[$j] + 1 .. @fastaseq - 1]);
+        }
+        else {
+            $line = join( q{}, @fastaseq[ $idx[$j] + 1 .. $idx[$j + 1] - 1]);
+        }
+        $line =~ s/[\n\r]//g;
+        $reference{$dsc[$j]} = lc $line;
+    }
+    return \%reference;
+}
+
+__END__
+
+
+=head1 NAME
+
+ name.pl - Short description
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 OPTIONS
+
+ name.pl [OPTION]... [FILE]...
+
+ -o, --output      filename to write results to (defaults to STDOUT)
+ -v, --verbose     output perl's diagnostic and warning messages
+ -q, --quiet       supress perl's diagnostic and warning messages
+ -h, --help        print this information
+ -m, --manual      print the plain old documentation page
+
+=head1 REVISION
+
+ Version 0.0.1
+
+ $Rev: $:
+ $Author: $:
+ $Date: $:
+ $HeadURL: $:
+ $Id: $:
+
+=head1 AUTHOR
+
+ Pedro Silva <psilva@nature.berkeley.edu/>
+ Zilberman Lab <http://dzlab.pmb.berkeley.edu/>
+ Plant and Microbial Biology Department
+ College of Natural Resources
+ University of California, Berkeley
+
+=head1 COPYRIGHT
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+=cut
---
+++ intersect_lists.pl
@@ -0,0 +1,180 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use Data::Dumper;
+use Carp;
+use Getopt::Long;
+use Pod::Usage;
+
+my $output;
+my $annotation;
+my $lists_tag = 'gene';
+my $genes_tag = 'ID';
+
+# Grabs and parses command line options
+my $result = GetOptions (
+    'annotation|a=s' => \$annotation,
+    'lists-tag|lt=s' => \$lists_tag,
+    'genes-tag|gt=s' => \$genes_tag,
+    'output|o=s'     => \$output,
+    'verbose|v'      => sub { use diagnostics; },
+    'quiet|q'        => sub { no warnings; },
+    'help|h'         => sub { pod2usage ( -verbose => 1 ); },
+    'manual|m'       => sub { pod2usage ( -verbose => 2 ); }
+);
+
+# Check required command line parameters
+pod2usage ( -verbose => 1 )
+unless $result and @ARGV;
+
+if ($output) {
+    open my $USER_OUT, '>', $output or croak "Can't open $output for writing: $!";
+    select $USER_OUT;
+}
+
+my $genes_regex = qr/$genes_tag[=\s]?([^;\s]+).*/;
+my $lists_regex = qr/$lists_tag[=\s]?.*\*([^;:\s]+).*/;
+
+my $genes         = index_genes ($annotation, $genes_regex);
+my $lists         = {};
+my $sets          = {};
+my $intersections = {};
+
+while (my $list = shift @ARGV) {
+
+    $lists->{$list} = index_genes ($list, $lists_regex);
+
+    for my $gene (sort keys %{$genes}) {
+        if (exists $lists->{$list}->{$gene}) {
+            push @{$genes->{$gene}{list}}, $list;
+            $sets->{$list}++;
+        }
+    }
+}
+
+my $output_files = {};
+
+for my $gene (sort keys %{$genes}) {
+    next unless @{$genes->{$gene}{list}};
+    my $combo = join q{,}, sort @{$genes->{$gene}{list}};
+    $intersections->{$combo}++;
+
+    $output_files->{$combo}
+    || open $output_files->{$combo}, '>', "$combo.gene-list.gff"
+    || croak "Can't write to $combo: $!";
+    
+    print {$output_files->{$combo}} "$genes->{$gene}{gff}\n";
+}
+
+print q{=} x 40, "\n";
+print "Genes per list:\n";
+print q{=} x 40, "\n";
+while (my ($list, $count) = each %$sets) {
+    print $list, "\t", $count, "\n";
+}
+print "\n";
+print q{=} x 40, "\n";
+print "Genes per intersection:\n";
+print q{=} x 40, "\n";
+while (my ($list, $count) = each %$intersections) {
+    print $list, "\t", $count, "\n";
+}
+
+
+
+
+sub index_genes {
+    my ($genes_file, $regex) = @_;
+
+    my $genes = {};
+
+    open my $GENES, '<', $genes_file or croak "Can't open $genes_file: $!";
+    while (<$GENES>) {
+        next if m/^\s*#/;
+        s/[\r\n]//g;
+        my @fields = split /\t/;
+
+        $fields[-1] =~ s/$regex/$1/;
+
+        $genes->{$fields[-1]}{list} = [];
+        $genes->{$fields[-1]}{gff} = $_;
+    }
+
+    return $genes;
+}
+
+
+
+__END__
+
+
+=head1 NAME
+
+ intersect_lists.pl - Given a GFF annotation file and a list of GFF data files, determine which data files contain which loci
+
+=head1 SYNOPSIS
+
+ intersect_lists.pl --genes TAIR8_genes.gff list_1.gff list_2.gff ... list_n.gff
+
+ intersect_lists.pl -g dmel_genes.gff list*gff --output dmel_lists_intersection.dat
+
+=head1 DESCRIPTION
+
+ Given a GFF annotation file and a list of GFF data files, determines the intersection of each data file and the loci in the annotation.
+
+ It requires a valid GFF version 3 file format, in which the last (9th) field--the attribute--may contain multiple TAG=VALUE pairs delimited by a semi-collon ':'.
+
+ It assumes by default that, for the GFF annotation file, that the field to compare is TAGged by 'ID=', but this can be changed via the --genes-tag option
+ (eg. --genes-tag 'transcript_id').
+
+ It assumes by default that the GFF data files have attributes delimited by 'gene=', that there may be multiple values per TAG delimited by a comma ',',
+ and that in that case the appropriate value will be determined by its having a '*' between the tag and the value (eg. gene=VALUE1,*VALUE2 will return VALUE2).
+
+=head1 OPTIONS
+
+ intersect_lists.pl [OPTION]... [FILES]...
+
+ -a,  --annotation  filename of loci GFF file
+ -lt, --lists-tag   GFF attribute identifier tag (default: ID)
+ -gt, --genes-tag   GFF attribute identifier tag (default: gene)
+ -o,  --output      filename to write results to (defaults to STDOUT)
+ -v,  --verbose     output perl's diagnostic and warning messages
+ -q,  --quiet       supress perl's diagnostic and warning messages
+ -h,  --help        print this information
+ -m,  --manual      print the plain old documentation page
+
+=head1 REVISION
+
+ Version 0.0.1
+
+ $Rev: $:
+ $Author: $:
+ $Date: $:
+ $HeadURL: $:
+ $Id: $:
+
+=head1 AUTHOR
+
+ Pedro Silva <pedros@berkeley.edu/>
+ Zilberman Lab <http://dzlab.pmb.berkeley.edu/>
+ Plant and Microbial Biology Department
+ College of Natural Resources
+ University of California, Berkeley
+
+=head1 COPYRIGHT
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+=cut
