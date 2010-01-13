d1d416a913dae7b041e80fef00a4535c8683cd80
Pedro Silva <pedros@berkeley.edu>
2010-01-13 02:30:38 +0000

    First working commit

--- window_gff_REFACTORED.pl
+++ window_gff_REFACTORED.pl
@@ -17,17 +17,19 @@ my $no_skip = 0;
 my $average = 0;
 my $gff;
 my $tag     = 'ID';
+my $feature;
 
 # Grabs and parses command line options
 my $result = GetOptions(
     'width|w=i'  => \$width,
     'step|s=i'   => \$step,
     'average|a'  => \$average,
-    'merge|m'    => \$merge,
+    'merge|m=s'  => \$merge,
     'no-sort|n'  => \$no_sort,
     'no-skip|k'  => \$no_skip,
     'gff|g=s'    => \$gff,
     'tag|t=s'    => \$tag,
+    'feature|f=s'=> \$feature,
     'output|o=s' => \$output,
     'verbose'    => sub { use diagnostics; },
     'quiet'      => sub { no warnings; },
@@ -37,7 +39,8 @@ my $result = GetOptions(
 
 # Check required command line parameters
 pod2usage( -verbose => 1 )
-    unless @ARGV and $result;
+unless @ARGV and $result
+and (($width and $step) or $gff);
 
 if ($output) {
     open my $USER_OUT, '>', $output
@@ -58,22 +61,17 @@ my %gff_records = ();
 LOAD:
 while ( my $gff_line = $gff_iterator->() ) {
 
-    # &gff_read returns [] for GFF comments, invalid lines, etc.
     next LOAD unless ref $gff_line eq 'HASH';
-
     push @{ $gff_records{ $gff_line->{seqname} } }, $gff_line;
-
 }
 
 my $window_iterator;
 
 if ($gff) {
-    $window_iterator = make_window_iterator(
-        gff => {
-            file  => $gff, 
-            tag   => $tag, 
-            merge => $merge
-        }
+    $window_iterator = make_annotation_iterator(
+        file  => $gff, 
+        tag   => $tag, 
+        merge => $merge
     );
 }
 
@@ -87,22 +85,18 @@ for my $sequence ( sort keys %gff_records ) {
 
     unless ($gff) {
         $window_iterator = make_window_iterator(
-            windows => {
-                width => $width,
-                step  => $step, 
-                lower => 1,
-                upper => $gff_records{$sequence}[-1]->{end}
-            }
+            width => $width,
+            step  => $step, 
+            lower => 1,
+            upper => $gff_records{$sequence}[-1]->{end}
         );
     }
 
   WINDOW:
     while ( my ($ranges, $locus) = $window_iterator->($sequence) ) {
 
-        # print STDERR "$start\t$end\n";next WINDOW;
-
         my $brs_iterator = binary_range_search(
-            range    => $ranges,
+            range    => [@$ranges], # make an anonymous copy because binary_range_search *will* modify references
             ranges   => $gff_records{$sequence},
         );
 
@@ -111,113 +105,150 @@ for my $sequence ( sort keys %gff_records ) {
             ? average_scores($brs_iterator)
             : fractional_methylation($brs_iterator);
 
-        my ( $score, $attribute ) = qw(. .);
+        my ( $score, $attribute );
 
         if ( ref $scores_ref eq 'HASH' ) {
 
             $score = sprintf( "%g", $scores_ref->{score} );
             delete $scores_ref->{score};
 
-            $attribute = join q{; }, map { "$_=" . $scores_ref->{$_} }
+            $attribute  = "ID=$locus; " if $locus;
+            $attribute .= join q{; }, map { "$_=" . sprintf("%g", $scores_ref->{$_}) }
                 sort keys %{$scores_ref};
 
         }
+        elsif ($no_skip) {
+            $score     = q{.};
+            $attribute = $locus ? "ID=$locus" : q{.};
+        }
+        else {
+            next WINDOW;
+        }
 
         print join( "\t",
-                    $sequence, 'dzlab',         "w$width",
-                    $start,        $end, $score,
-                    q{.},      q{.},            $attribute,
-                ),
-                "\n";
-
+                    $sequence, 'dzlab', ($feature ? $feature : $gff ? 'locus' : 'window'),
+                    $ranges->[0][0], $ranges->[-1][1], $score,
+                    q{.}, q{.}, $attribute,
+                ), "\n";
     }
 
     delete $gff_records{$sequence};
 }
 
+
+
 sub make_window_iterator {
     my (%options) = @_;
 
-    if ($options{windows}) {
-        my $width = $options{windows}{width};
-        my $step  = $options{windows}{step};
-        my $lower = $options{windows}{lower};
-        my $upper = $options{windows}{upper};
+    my $width = $options{width} || croak 'Need window parameter';
+    my $step  = $options{step}  || croak 'Need step parameter';
+    my $lower = $options{lower} || croak 'Need lower bound parameter';
+    my $upper = $options{upper} || croak 'Need upper bound parameter';
         
-        return sub {
-            my $i      = $lower;
-            $lower    += $step;
+    return sub {
+        my $i      = $lower;
+        $lower    += $step;
 
-            if ($i <= $upper - $width + 1) {
-                return [ [$i, $i + $width - 1] ], "w$step";
-            }
-            else {
-                return undef;
-            }
+        if ($i <= $upper - $width + 1) {
+            return [ [$i, $i + $width - 1] ];
+        } 
+        else {
+            return;
         }
     }
-    elsif ($options{gff}) {
+}
 
-        my $annotation_file = $options{gff}{file};
-        my $locus_tag       = $options{gff}{tag}   || 'ID';
-        my $merge_exons     = $options{gff}{merge} || 0;
+sub make_annotation_iterator {
+    my (%options) = @_;
+
+    my $annotation_file = $options{file};
+    my $locus_tag       = $options{tag}   || 'ID';
+    my $merge_exons     = $options{merge} || 0;
         
-        open my $GFFH, '<', $annotation_file 
-        or croak "Can't read file: $annotation_file";
+    open my $GFFH, '<', $annotation_file 
+    or croak "Can't read file: $annotation_file";
 
-        my $gff_iterator
-        = make_gff_iterator( parser => \&gff_read, handle => $GFFH );
+    my $gff_iterator
+    = make_gff_iterator( parser => \&gff_read, handle => $GFFH );
 
-        my %annotation = ();
+    my $annotation 
+    = index_annotation( iterator => $gff_iterator, %options );
 
-      LOCUS:
-        while ( my $locus = $gff_iterator->() ) {
+    close $GFFH or croak "Can't close $annotation_file: $!";
 
-            next LOCUS unless ref $locus eq 'HASH';
+    my %annotation_keys = ();
 
-            my ($locus_id) = $locus->{attribute} =~ m/$locus_tag[=\s]?([^;]+)/;
+    return sub {
+        my ($sequence) = @_;
+        return unless $sequence;
 
-            if (!defined $locus_id) {
-                ($locus_id, undef) = split /;/, $locus->{attribute};
-                $locus_id ||= q{.};
-            } 
-            else {
-                $locus_id =~ s/["\t\r\n]//g;
-                $locus_id =~ s/\.\d$// if $merge_exons;
-            }
-
-            # push @{ $annotation{$locus->{seqname}}{$locus->{start}}},
-            # [$locus->{start}, $locus->{end}, $locus_id, $locus->{strand}, $locus->{source}, $locus->{feature}, $locus->{attribute}];
-
-            push @{ $annotation{$locus->{seqname}}{$locus_id} },
-            [$locus->{start}, $locus->{end}];
-
+        unless (exists $annotation_keys{$sequence}) {
+            @{ $annotation_keys{$sequence} }
+            = sort keys %{$annotation->{$sequence}};
         }
-        close $GFFH;
 
-        my %annotation_keys;
+        my $locus  = shift @{ $annotation_keys{$sequence} };
+        return unless $locus;
+        my $ranges = $annotation->{$sequence}{$locus}; delete $annotation->{$sequence}{$locus};
 
-        return sub {
-            my ($sequence) = @_;
-            return unless $sequence;
+        if ($locus and @$ranges) {
+            return [uniq_ranges ($ranges)], $locus;
+        } 
+        else {
+            return;
+        }
+    };
+}
 
-            unless (exists $annotation_keys{$sequence}) {
-                @{ $annotation_keys{$sequence} }
-                = sort {$annotation{$sequence}{$a}->[0] <=> $annotation{$sequence}{$b}->[0]}
-                    keys %{$annotation{$sequence}};
-            }
 
-            my $locus  = shift @{ $annotation_keys{$sequence} };
-            my $ranges = shift @{ $annotation{$sequence}{$locus} };
+sub index_annotation {
+    my (%options) = @_;
 
-            if ($locus and @$ranges) {
-                return $ranges, $locus;
-            }
-            else {
-                return undef;
-            }
-        };
+    my $gff_iterator  = $options{iterator} || croak 'Need an iterator parameter';
+    my $locus_tag     = $options{tag}      || 'ID';
+    my $merge_feature = $options{merge}    || 0;
+
+    my %annotation    = ();
+
+  LOCUS:
+    while ( my $locus = $gff_iterator->() ) {
+
+        next LOCUS unless ref $locus eq 'HASH';
+
+        my ($locus_id) = $locus->{attribute} =~ m/$locus_tag[=\s]?([^;,]+)/;
+
+        if (!defined $locus_id) {
+            ($locus_id, undef) = split /;/, $locus->{attribute};
+            $locus_id ||= q{.};
+        } 
+        else {
+            $locus_id =~ s/["\t\r\n]//g;
+            $locus_id =~ s/\.\d$// # this fetches the parent ID in GFF gene models (eg. exon is Parent=ATG101010.n)
+            if $merge_feature and $locus->{feature} eq $merge_feature;
+        }
+
+        push @{ $annotation{$locus->{seqname}}{$locus_id} },
+        [$locus->{start}, $locus->{end}] 
+        unless ($merge_feature and $locus->{feature} ne $merge_feature);
+
     }
+
+    return \%annotation;
+}
+
+
+
+sub uniq_ranges {
+    my ($ranges) = @_;
+
+    my %seen;
+    my @uniq;
+
+    for (@$ranges) {
+        push @uniq, $_ unless $seen{"$_->[0];$_->[1]"}++;
+    }
+
+    return wantarray ? @uniq : [@uniq];
 }
 
 
@@ -289,18 +320,12 @@ COORD:
     }
 }
 
-sub range_assert {
-    my ( $coord, $low, $high ) = @_;
-
-    if ( $coord < $low or $coord > $high ) {
-        croak "NOT OK: $coord\t$low-$high\n";
-    }
-}
 
 sub binary_range_search {
     my %options = @_;
-    my $rage     = $options{range}  || return;
-    my $ranges   = $options{ranges} || return;
+
+    my $range  = shift @{$options{range}} || croak 'Need a range parameter';
+    my $ranges = $options{ranges}         || croak 'Need a ranges parameter';
 
     my ( $low, $high ) = ( 0, @{$ranges} - 1 );
 
@@ -315,7 +340,8 @@ sub binary_range_search {
 
         my %seen = ();
 
-        my $brs_iterator = sub {
+        my $brs_iterator = sub { };
+        $brs_iterator = sub {
 
             if (    $ranges->[ $up + 1 ]{end}       >= $range->[0]
                     and $ranges->[ $up + 1 ]{start} <= $range->[1]
@@ -336,6 +362,14 @@ sub binary_range_search {
                 $seen{$try} = undef;
                 return $ranges->[$try];
             }
+            elsif (@{$options{range}}) {
+                $brs_iterator = 
+                binary_range_search(
+                    range    => $options{range},
+                    ranges   => $options{ranges},
+                );
+                return $brs_iterator->();
+            }
             else {
                 return;
             }
@@ -345,6 +379,7 @@ sub binary_range_search {
     }
     return sub { };
 }
+
 
 sub gff_read {
     return [] if $_[0] =~ m/^
@@ -381,13 +416,12 @@ sub make_gff_iterator {
     croak
         "Need parser function reference and file name or handle to build iterator"
         unless $parser
-            and ref $parser eq 'CODE'
-            and (
-                ( defined $file and -e $file )
-                xor(defined $GFF_HANDLE and ref $GFF_HANDLE eq 'GLOB'
-                        or $GFF_HANDLE eq 'ARGV'
-                )
-            );
+        and ref $parser eq 'CODE'
+        and ( ( defined $file and -e $file )
+              xor (defined $GFF_HANDLE and ref $GFF_HANDLE eq 'GLOB'
+                   or $GFF_HANDLE eq 'ARGV'
+               )
+          );
 
     if ($file) {
         open $GFF_HANDLE, '<', $file
@@ -403,16 +437,47 @@ __END__
 
 =head1 NAME
 
- name.pl - Short description
+ window_gff.pl - Average GFFv3 data over a sliding window or against a GFFv3 annotation file
 
 =head1 SYNOPSIS
 
+ # Run a sliding window of 50bp every 25bp interval on methylation data (attribute field assumed to be 'c=n; t=m')
+ window_gff.pl --width 50 --step 25 --output out.gff in.gff
+
+ # Average data per each locus (on score field)
+ window_gff.pl --gff genes.gff --average --output out.gff in.gff
+
+ # Merge 'exon' features in GFF annotation file per parent ID (eg. per gene)
+ window_gff.pl --gff exons.gff --average --output out.gff --tag Parent --merge exon
+
 =head1 DESCRIPTION
+
+ This program works in two modes. It will either run a sliding window through the input GFF data,
+ or load a GFF annotation file and use that to average scores in input GFF data.
+
+ Scores calculation by default is done via fractional methylation, in which case the program
+ requires that the GFF attribute field contain both 'c' and 't' tags (eg. 'c=n; t=m').
+ Alternatively, the -a, --average switch will make the program average the scores in the score field.
+
+ An additional switch, -m, --merge, will take the name of a feature in the annotation GFF file.
+ It will then try to average out all the features that have a common parent locus.
+ This assumes that, for example if averaging exon features: the user must use '--merge exon',
+ '--tag Parent' (in the case of Arabidopsis' annotations), which will fetch a gene ID like ATG01010.1.
+ It is assumed that the Parent feature of this exon is ATG010101.
 
 =head1 OPTIONS
 
- name.pl [OPTION]... [FILE]...
-
+ window_gff.pl [OPTION]... [FILE]...
+ 
+ -w, --width       sliding window width                                 (default: 50, integer)
+ -s, --step        sliding window interval                              (default: 50, integer)
+ -a, --average     average scores in GFF score field                    (default: no)
+ -m, --merge       merge this feature as belonging to same locus        (default: no, string [eg: exon])
+ -n, --no-sort     GFFv3 data assumed sorted by start coordinate        (default: no)
+ -k, --no-skip     print windows or loci for which there is no coverage (deftaul: no)
+ -g, --gff         GFFv3 annotation file
+ -t, --tag         attribute field tag from which to extract locus ID   (default: ID, string)
+ -f, --feature     overwrite GFF feature field with this label          (default: no, string)
  -o, --output      filename to write results to (defaults to STDOUT)
  -v, --verbose     output perl's diagnostic and warning messages
  -q, --quiet       supress perl's diagnostic and warning messages
@@ -453,10 +518,3 @@ __END__
  along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 =cut
-222:	final indentation level: 1
-
-Final nesting depth of '{'s is 1
-The most recent un-matched '{' is on line 87
-87: sub binary_range_search {
-                            ^
-222:	To save a full .LOG file rerun with -g
