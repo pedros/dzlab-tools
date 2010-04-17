d1cd67f83898780974594f212b5b6fc73d6d830a
Pedro Silva <pedros@berkeley.edu>
2010-04-17 00:09:56 +0000

    First commit

---
+++ plot_kde.pl
@@ -0,0 +1,101 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use Data::Dumper;
+use Carp;
+use Getopt::Long;
+use Pod::Usage;
+use Statistics::KernelEstimation;
+
+my $DATA_HANDLE = 'ARGV';
+my $output;
+
+# Grabs and parses command line options
+my $result = GetOptions (
+    'output|o=s'  => \$output,
+    'verbose|v'   => sub { use diagnostics; },
+    'quiet|q'     => sub { no warnings; },
+    'help|h'      => sub { pod2usage ( -verbose => 1 ); },
+    'manual|m'    => sub { pod2usage ( -verbose => 2 ); }
+);
+
+# Check required command line parameters
+pod2usage ( -verbose => 1 )
+unless @ARGV and $result;
+
+if ($output) {
+    open my $USER_OUT, '>', $output or croak "Can't open $output for writing: $!";
+    select $USER_OUT;
+}
+
+my $s = Statistics::KernelEstimation->new();
+
+while (<>) {
+    chomp;
+    $s->add_data($_);
+}
+
+my $w           = $s->default_bandwidth();
+my ($min, $max) = $s->extended_range();
+
+for( my $x = $min; $x <= $max; $x += ($max - $min) / 100 ) {
+    print $x, "\t", $s->pdf( $x, $w ), "\t", $s->cdf( $x, $w ), "\n";
+}
+
+
+__END__
+
+
+=head1 NAME
+
+ plot_kde.pl - Compute KDE and-or CDFs
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 OPTIONS
+
+ plot_kde.pl [OPTION]... [FILE]...
+
+ -o, --output      filename to write results to (defaults to STDOUT)
+ -v, --verbose     output perl's diagnostic and warning messages
+ -q, --quiet       supress perl's diagnostic and warning messages
+ -h, --help        print this information
+ -m, --manual      print the plain old documentation page
+
+=head1 REVISION
+
+ Version 0.0.1
+
+ $Rev: $:
+ $Author: $:
+ $Date: $:
+ $HeadURL: $:
+ $Id: $:
+
+=head1 AUTHOR
+
+ Pedro Silva <psilva@nature.berkeley.edu/>
+ Zilberman Lab <http://dzlab.pmb.berkeley.edu/>
+ Plant and Microbial Biology Department
+ College of Natural Resources
+ University of California, Berkeley
+
+=head1 COPYRIGHT
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+=cut
