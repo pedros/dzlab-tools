69a346d4af02d6a27359627e4b6f67fb4e486788
Pedro Silva <pedros@berkeley.edu>
2010-10-08 21:18:27 +0000

    first commit

---
+++ ensure_all_genes_present.pl
@@ -0,0 +1,185 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+use Data::Dumper;
+use Carp;
+use Getopt::Long;
+use Pod::Usage;
+use version; our $VERSION = qv('0.0.1');
+
+GetOptions(
+    \%ARGV,
+    'input|i=s', 'output|o=s', 'error|e=s',
+    'annotation|a=s',
+    _meta_options( \%ARGV ),
+) and (@ARGV or $ARGV{input}) or pod2usage( -verbose => 1 );
+
+my ( $INH, $OUTH, $ERRH ) = _prepare_io( \%ARGV, \@ARGV );
+
+my %genes;
+open my $ANNOTATION, q{<}, $ARGV{annotation} or die "Can't open $ARGV{annotation}: $!";
+
+LOAD_CANONICAL_GENE_LIST:
+while (<$ANNOTATION>) {
+    chomp;
+    my ($id) = $_ =~ m/ID=([^;]+)/;
+    $genes{$id} = 0;
+}
+close $ANNOTATION or die "Can't close $ARGV{annotation}: $!";
+
+my @genes = sort keys %genes;
+
+SCORES_FILE:
+for my $scores (@ARGV) {
+    my %file_genes;
+    open my $SCORES, q{<}, $scores or die "Can't open $scores: $!";
+
+  SCORE:
+    while (<$SCORES>) {
+        chomp;
+        my ($id, $sc, $alt) = split /\t/; $alt ||= '';
+
+        $file_genes{$id} = join "\t", $sc, $alt;
+    }
+    close $SCORES or die "Can't close $scores: $!";
+
+    open my $OUTFILE, q{>}, $scores or die "Can't open $scores: $!";
+  CANONICAL_GENE:
+    for (@genes) {
+        if (exists $file_genes{$_}) {
+            print $OUTFILE "$_\t$file_genes{$_}\n";
+        }
+        else {
+            print $OUTFILE "$_\t$genes{$_}\n";
+        }
+    }
+    close $OUTFILE or die "Can't close $scores: $!";
+}
+
+
+
+
+sub _meta_options {
+    my ($opt) = @_;
+
+    return (
+        'quiet'     => sub { $opt->{quiet}   = 1;          $opt->{verbose} = 0 },
+        'verbose:i' => sub { $opt->{verbose} = $_[1] // 1; $opt->{quiet}   = 0 },
+        'version'   => sub { pod2usage( -sections => ['VERSION', 'REVISION'],
+                                        -verbose  => 99 )                      },
+        'license'   => sub { pod2usage( -sections => ['AUTHOR', 'COPYRIGHT'],
+                                        -verbose  => 99 )                      },
+        'usage'     => sub { pod2usage( -sections => ['SYNOPSIS'],
+                                        -verbose  => 99 )                      },
+        'help'      => sub { pod2usage( -verbose  => 1  )                      },
+        'manual'    => sub { pod2usage( -verbose  => 2  )                      },
+    );
+}
+
+sub _prepare_io {
+    my ($opt, $argv) = @_;
+
+    my ($INH, $OUTH, $ERRH);
+    
+    # If user explicitly sets -i, put the argument in @$argv
+    unshift @$argv, $opt->{input} if exists $opt->{input};
+
+    # Allow in-situ arguments (equal input and output filenames)
+    if (    exists $opt->{input} and exists $opt->{output}
+               and $opt->{input} eq $opt->{output} ) {
+        open $INH, q{<}, $opt->{input}
+            or croak "Can't read $opt->{input}: $!";
+        unlink $opt->{output};
+    }
+    else { $INH = *ARGV }
+
+    # Redirect STDOUT to a file if so specified
+    if ( exists $opt->{output} and q{-} ne $opt->{output} ) {
+        open $OUTH, q{>}, $opt->{output}
+            or croak "Can't write $opt->{output}: $!";
+    }
+    else { $OUTH = *STDOUT }
+
+    # Log STDERR if so specified
+    if ( exists $opt->{error} and q{-} ne $opt->{error} ) {
+        open $ERRH, q{>}, $opt->{error}
+            or croak "Can't write $opt->{error}: $!";
+    }
+    elsif ( exists $opt->{quiet} and $opt->{quiet} ) {
+        use File::Spec;
+        open $ERRH, q{>}, File::Spec->devnull
+            or croak "Can't write $opt->{error}: $!";
+    }
+    else { $ERRH = *STDERR }
+
+    return ( $INH, $OUTH, *STDERR = $ERRH );
+}
+
+__DATA__
+
+
+__END__
+
+=head1 NAME
+
+ensure_all_genes_present.pl - Ensure all genes present
+
+=head1 SYNOPSIS
+
+ensure_all_genes_present.pl [OPTION]... [[-i] FILE]...
+
+ensure_all_genes_present.pl bunch_of_'GENEID SCORE'_FILES
+
+=head1 DESCRIPTION
+
+ Long description
+
+=head1 OPTIONS
+
+ -i, --input       <string>     input filename                           (STDIN)
+ -o, --output      <string>     output filename                          (STDOUT)
+ -e, --error       <string>     output error filename                    (STDERR)
+     --verbose     [integer]    print increasingly verbose error messages
+     --quiet                    print no diagnostic or warning messages
+     --version                  print current version
+     --license                  print author's contact and copyright information
+     --help                     print this information
+     --manual                   print the plain old documentation page
+
+=head1 VERSION
+
+ 0.0.1
+
+=head1 REVISION
+
+ $Rev: $:
+ $Author: $:
+ $Date: $:
+ $HeadURL: $:
+ $Id: $:
+
+=head1 AUTHOR
+
+ Pedro Silva <pedros@berkeley.edu/>
+ Zilberman Lab <http://dzlab.pmb.berkeley.edu/>
+ Plant and Microbial Biology Department
+ College of Natural Resources
+ University of California, Berkeley
+
+=head1 COPYRIGHT
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+=cut
