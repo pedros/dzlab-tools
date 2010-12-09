d11d732b04fdc6cb4b8aba14af7c6a7ae90182fd
Toshiro Nishimura <tnish@jabberwock.dzlab.pmb.berkeley.edu>
2010-12-09 15:57:19 -0800

    DZLab::Tools::RunUtils created to factor out _prepare_io and _meta_options. sort_gff.pl, was used as the first guinea-pig for the refactoring. also added t/ directory for testing all such changes.

---
+++ DZLab-Tools/lib/DZLab/Tools/RunUtils.pm
@@ -0,0 +1,87 @@
+package DZLab::Tools::RunUtils;
+use version; our $VERSION = qv('0.0.1');
+use strict;
+use warnings;
+use Carp;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw();
+our @EXPORT = qw(_meta_options _prepare_io);
+
+# average_gff.pl
+# build_gff_score_table.pl
+# check_methyl.pl
+# eland2gff.pl
+# ensure_all_genes_present.pl
+# expand_gff.pl
+# gff2eland.pl
+# gff2eland3.pl
+# normalize_gff.pl
+# * sort_gff.pl
+sub _meta_options {
+    my ($opt) = @_;
+
+    return (
+        'quiet'     => sub { $opt->{quiet}   = 1;          $opt->{verbose} = 0 },
+        'verbose:i' => sub { $opt->{verbose} = $_[1] // 1; $opt->{quiet}   = 0 },
+        'version'   => sub { pod2usage( -sections => ['VERSION', 'REVISION'],
+                                        -verbose  => 99 )                      },
+        'license'   => sub { pod2usage( -sections => ['AUTHOR', 'COPYRIGHT'],
+                                        -verbose  => 99 )                      },
+        'usage'     => sub { pod2usage( -sections => ['SYNOPSIS'],
+                                        -verbose  => 99 )                      },
+        'help'      => sub { pod2usage( -verbose  => 1  )                      },
+        'manual'    => sub { pod2usage( -verbose  => 2  )                      },
+    );
+}
+
+# average_gff.pl
+# build_gff_score_table.pl
+# check_methyl.pl
+# eland2gff.pl
+# ensure_all_genes_present.pl
+# expand_gff.pl
+# gff2eland.pl
+# gff2eland3.pl
+# normalize_gff.pl
+# parent_imprinting.pl
+# * sort_gff.pl
+sub _prepare_io {
+    my ($opt, $argv) = @_;
+
+    my ($INH, $OUTH, $ERRH);
+    
+    unshift @$argv, $opt->{input} if exists $opt->{input};
+
+    if (    exists $opt->{input} and exists $opt->{output}
+               and $opt->{input} eq $opt->{output} ) {
+        open $INH, q{<}, $opt->{input}
+            or croak "Can't read $opt->{input}: $!";
+        unlink $opt->{output};
+    }
+    else { $INH = *ARGV }
+
+    if ( exists $opt->{output} and q{-} ne $opt->{output} ) {
+        open $OUTH, q{>}, $opt->{output}
+            or croak "Can't write $opt->{output}: $!";
+    }
+    else { $OUTH = *STDOUT }
+
+    if ( exists $opt->{error} and q{-} ne $opt->{error} ) {
+        open $ERRH, q{>}, $opt->{error}
+            or croak "Can't write $opt->{error}: $!";
+    }
+    elsif ( exists $opt->{quiet} and $opt->{quiet} ) {
+        use File::Spec;
+        open $ERRH, q{>}, File::Spec->devnull
+            or croak "Can't write $opt->{error}: $!";
+    }
+    else { $ERRH = *STDERR }
+
+    return ( $INH, $OUTH, *STDERR = $ERRH );
+}
+
+1;
+
+
---
+++ t/t-sort_gff.t
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Test::Simple qw(no_plan);
+#use Test::More qw(no_plan);
+use List::Util 'shuffle';
+
+my $orig = 't/sorted.gff';
+my $dirty = 't/unsorted.gff';
+my $test = 't/test.gff';
+
+shuffle_file($orig,$dirty);
+system("./sort_gff.pl -i $dirty -o $test");
+
+ok(files_identical($test,$orig));
+
+unlink $test;
+unlink $dirty;
+
+sub shuffle_file{
+    my ($in,$out) = @_;
+    open my $inh, '<', $in;
+    open my $outh, '>', $out;
+    print $outh shuffle(<$inh>);
+    close $outh;
+    close $inh;
+}
+
+sub files_identical{
+    local $/=undef;
+    my ($f1,$f2) = @_;
+    open my $f1h, '<', $f1;
+    open my $f2h, '<', $f2;
+    my $s1 = <$f1h>;
+    my $s2 = <$f2h>;
+    close $f1h;
+    close $f2h;
+    return $s1 eq $s2;
+}
