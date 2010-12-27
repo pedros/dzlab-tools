e8a909a100c7a9c66189edeb2cd4aff71e7c0b3d
Toshiro Nishimura <tnish@berkeley.edu>
2010-12-27 14:22:32 -0800

    gff2sqlite.pl working

--- utilities/gff2sqlite.pl
+++ utilities/gff2sqlite.pl
@@ -5,15 +5,14 @@ use Data::Dumper;
 use feature 'say';
 
 use FindBin;
-use lib "$FindBin::Bin/DZLab-Tools/lib";
+use lib "$FindBin::Bin/../DZLab-Tools/lib";
 use DZLab::Tools::GFF qw/gff_make_iterator/;
 use DZLab::Tools::GFFStore;
 
 my $gffstore = DZLab::Tools::GFFStore->new({
-        attributes => {c => 'numeric', t => 'numeric', n => 'numeric'},
-        verbose => 1, 
-        debug => 1,
-        indices => [['start','end']],
-        handle => \*ARGV,
+        attributes => {ID => 'text', Parent => 'text'},
+        verbose => 1,
     });
+
+$gffstore->slurp({handle => \*ARGV});
 
