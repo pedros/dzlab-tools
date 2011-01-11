30fba9ffdcb2f4a0eee329cdbca63a583141cd71
Pedro Silva <pedros@berkeley.edu>
2011-01-11 12:25:53 -0800

    add bottom-up scaffolding
    * make_mates_iterator()
    * check_mates()
    * choose_mates()

--- merge_sam.pl
+++ merge_sam.pl
@@ -19,6 +19,7 @@ GetOptions(
 
 my ( $INH, $OUTH, $ERRH ) = _prepare_io( \%ARGV, \@ARGV );
 
+
 my ($left, $right) = map {
     Bio::DB::Sam->new( 
         -bam => $_,
@@ -27,9 +28,12 @@ my ($left, $right) = map {
     )->features( -iterator => 1 ) 
 } @ARGV[0, 1];
 
-while (my @alignments = next_multiple_alignments( $left ) ) {
 
+while (my $alignments = next_multiple_alignments( $left ) ) {
+    #die Dumper $alignments->[0]->strand;
+    print Dumper $alignments->[0]->strand;
 }
+
 
 
 
@@ -46,6 +50,104 @@ while (my @alignments = next_multiple_alignments( $left ) ) {
 
     ##### Multiple matches on both ends #####
 
+
+=head2 choose_mates
+
+ Takes an iterator of two Bio::DB::Bam::AlignWrapper objects as produced by make_mates_iterator(),
+ and an options hash with the following keys:
+
+ %opt = (
+     random => boolean,
+     insert-size => integer,
+     variance    => float coefficient (0.0-1.0)
+     orientation => -1|1,
+ );
+
+ Returns best pair if unique possible mapping,
+ a random pair if multiple possible mappings and $opt{random},
+ or undef if no possible mappings exist.
+
+=cut
+
+sub choose_mates {
+    my ($alignment_iterator, %opts) = @_;
+
+    my @alignments;
+
+    while ( my $mates = $alignment_iterator->() ) {
+        push @alignments, $mates
+        if check_mates( @$mates, %opts );
+    }
+    
+    return shift @alignments if @alignments == 1;
+    return $alignments[int rand (@alignments - 1)] if @alignments > 1 and $opts{random};
+    return unless @alignments;
+}
+
+
+=head2 check_mates
+
+ Takes two Bio::DB::Sam::AlignWrapper objects,
+ and an options hash with the following keys:
+
+ %opt = (
+     insert-size => integer,
+     variance    => float coefficient (0.0-1.0)
+     orientation => -1|1,
+ );
+
+ Returns true if the Bio::DB::Sam::AlignWrapper objects:
+
+ 1. Map to same sequence ID (chromosome)
+ 2. Map to opposite (-1) or same (1) orientation
+ 3. Are separated, from end of one object to start of another,
+    by exactly $insert-size bp * ( +/- $variance )
+
+=cut
+
+sub check_mates {
+    my ($left, $right, %opts) = @_;
+
+    return if $left->get_tag_values('UNMAPPED') or $right->get_tag_values('UNMAPPED');
+
+    return unless $left->seq_id eq $right->seq_id                        # condition 1, map to same sequence
+    and           $left->strand *  $right->strand == $opts{orientation}; # condition 2, map according to $opts{orientation}
+
+    # HERE BE DRAGONS, breaking encapsulation when accessing {sam}.
+    # accessing $left or $right is the same after the check above.
+    my $chr_len = $left->{sam}->length($left->seq_id);
+
+    my $insert = $opts{orientation} == 1
+                 ? $right->start - $left->end
+                 : abs( $chr_len - $right->start + 1 ) - $left->end;
+
+    my $variance = $opts{distance} * $opts{variance};
+
+    return ($opts{distance} + $variance) >= $insert
+    and    ($opts{distance} - $variance) <= $insert; # condition 3, map within acceptable range
+}
+
+=head2 make_mates_iterator
+
+ Takes two references to arrays of Bio::DB::Bam::AlignWrapper objects.
+ Returns an anonymous subroutine that will iterate over all combinations
+ of one object from each array reference.
+
+=cut
+
+sub make_mates_iterator {
+    my ($left, $right) = @_;
+
+    my @combos;
+
+    while (my $l = shift @$left) {
+        for my $r (@$right) {
+            push @combos, [$l, $r];
+        }
+    }
+
+    return sub { shift @combos };
+}
 
 
 =head2 next_multiple_alignments
@@ -85,9 +187,9 @@ sub next_multiple_alignments {
                 push @{$buffer{$sam}}, $read;
             }
             else {
-                my @reads         = @{$buffer{$sam}};
+                my @reads        = @{$buffer{$sam}};
                 @{$buffer{$sam}} = $read;
-                return @reads
+                return \@reads
             }
         }
     }
