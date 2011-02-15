687f440e06f6fc199fd614bc1b1c3b85da4cc0b2
Pareto <Pareto@example.org>
2011-02-14 22:54:45 -0800

    testing Getopt::Euclid on parse_fasta.pl

--- parse_fasta.pl
+++ parse_fasta.pl
@@ -1,5 +1,4 @@
 #!/usr/bin/env perl
-
 use warnings;
 use strict;
 use Data::Dumper;
@@ -7,49 +6,29 @@ use Carp;
 use Getopt::Long;
 use Pod::Usage;
 use File::Basename;
+use Getopt::Euclid qw( :vars<opt_> );
+use Pod::Usage;
+
+pod2usage(-verbose => 99,-sections => [qw/NAME SYNOPSIS OPTIONS/]) 
+if $opt_help || !$opt_fasta_file;
 
 use FindBin;
 use lib "$FindBin::Bin/DZLab-Tools/lib";
 use DZLab::Tools::Fasta;
 
-my @range;
-my $seqid;
-my $output;
-my $list;
-my $split;
-my $rc;
 
-# Grabs and parses command line options
-my $result = GetOptions (
-    'list|l'       => \$list,
-    'seqid|i=s'    => \$seqid,
-    'rc'           => \$rc,
-    'range|r=i{2}' => \@range,
-    'split|s'      => \$split,
-    'output|o=s'   => \$output,
-    'verbose|v'    => sub { use diagnostics; },
-    'quiet|q'      => sub { no warnings; },
-    'help|h'       => sub { pod2usage ( -verbose => 1 ); },
-    'manual|m'     => sub { pod2usage ( -verbose => 2 ); }
-);
-
-# Check required command line parameters
-pod2usage ( -verbose => 1 )
-unless @ARGV;
-
-if ($output) {
-    open my $USER_OUT, '>', $output or croak "Can't open $output for writing: $!";
+if ($opt_output) {
+    open my $USER_OUT, '>', $opt_output or croak "Can't open $opt_output for writing: $!";
     select $USER_OUT;
 }
 
-my $reference = $ARGV[0];
-my %reference = %{ slurp_fasta ($reference) };
+my %reference = %{ slurp_fasta ($opt_fasta_file) };
 
 my $total_bp     = 0;
 my $total_non_bp = 0;
 
-if ($list) {
-    print $reference, ":\n";
+if ($opt_list) {
+    print $opt_fasta_file, ":\n";
 
     for (sort keys %reference) {
 
@@ -71,61 +50,58 @@ if ($list) {
 }
 
 
-if ($seqid) {
+if ($opt_seqid) {
 
-    if (-e $seqid) {
+    if (-e $opt_seqid) {
         
-        open my $SEQID, '<', $seqid or croak "Can't open $seqid for reading";
+        open my $SEQID, '<', $opt_seqid or croak "Can't open $opt_seqid for reading";
 
-      SEQID:
-        while ($seqid = <$SEQID>) {
-            $seqid =~ s/[\n\r]//g;
-            
-            my $sequence = _get_subseq (\%reference, $seqid, $rc);
+        SEQID:
+        while ($opt_seqid = <$SEQID>) {
+            $opt_seqid =~ s/[\n\r]//g;
 
-	    next SEQID unless $sequence;
+            my $sequence = _get_subseq (\%reference, $opt_seqid, $opt_rc);
 
-            @range = (1, length $sequence)
-            unless @range;
+            next SEQID unless $sequence;
 
-            my $file_name = fileparse ($reference);
+            my ($start,$end) = %opt_range ? @opt_range{'start','end'} : (1, length $sequence);
+
+            my $file_name = fileparse ($opt_fasta_file);
 
             # one time masking. keeping it here in case it's needed in the future
-            # if ($seqid =~ m/^scaffold_2$/) {
+            # if ($opt_seqid =~ m/^scaffold_2$/) {
             #     print STDERR length $sequence, "\n";
             #     my $n = 'N' x (2350000 - 1650000);
             #     substr $sequence, (1650000 - 1), (2350000 - 1650000), $n;
             # }
-            # elsif ($seqid =~ m/^scaffold_175$/) {
+            # elsif ($opt_seqid =~ m/^scaffold_175$/) {
             #     print STDERR length $sequence, "\n";
             #     my $n = 'N' x (56000 - 46000);
             #     substr $sequence, (46000 - 1), (56000 - 46000), $n;
             # }
 
-            print ">$seqid\n";
+            print ">$opt_seqid\n";
             print $sequence, "\n";
         }
     }
     else {
 
-        my $sequence = _get_subseq (\%reference, $seqid, $rc, @range);
+        my $sequence = _get_subseq (\%reference, $opt_seqid, $opt_rc, %opt_range);
 
-        @range = (1, length $sequence)
-        unless @range;
+        my ($start,$end) = %opt_range ? @opt_range{'start','end'} : (1, length $sequence);
 
-        my $file_name = fileparse ($reference);
+        my $file_name = fileparse ($opt_fasta_file);
 
-        print format_fasta("lcl|$file_name|$seqid|$range[0]-$range[1]", $sequence), "\n";
-        #print string_to_fasta($sequence, "lcl|$file_name|$seqid|$range[0]-$range[1]"), "\n";
+        print format_fasta("lcl|$file_name|$opt_seqid|$start-$end", $sequence), "\n";
 
         exit 0;
     }
 }
 
 
-if ($split) {
+if ($opt_split) {
     for my $chr (sort keys %reference) {
-        open my $CHROUT, '>', "$reference-$chr" or croak "Can't write to $reference-$chr";
+        open my $CHROUT, '>', "$opt_fasta_file-$chr" or croak "Can't write to $opt_fasta_file-$chr";
 
         print $CHROUT ">$chr\n";
         print $CHROUT $reference{$chr}, "\n";
@@ -166,41 +142,84 @@ sub _get_subseq {
     }
 }
 
-__END__
 
 =head1 NAME
 
- parse_fasta.pl - Retrieve sequence information from fasta files
+parse_fasta.pl - Retrieve sequence information from fasta files
 
 =head1 SYNOPSIS
 
- # list all sequences in fasta file:
+list all sequences in fasta file:
+
  parse_fasta.pl -l fasta.txt
 
- # print base pair 123 to 456 in sequence chr1
+print base pair 123 to 456 in sequence chr1:
+
  parse_fasta.pl -r 123 456 -i chr1 fasta.txt
 
- # retrieves, reverse compliments base pair 123 to 456 in sequence chr1
- # NOTE: all coordinates are relative to the 5' end of the + strand
- parse_fasta.pl --rc - -r 123 456 -i chr1 fasta.txt
+retrieves, reverse compliments base pair 123 to 456 in sequence chr1
+(NOTE: all coordinates are relative to the 5' end of the + strand):
 
-
-=head1 DESCRIPTION
+ parse_fasta.pl --rc -r 123 456 -i chr1 fasta.txt
 
 =head1 OPTIONS
 
- parse_fasta.pl [OPTION]... [FILE]...
+=over 1
 
- -l, --list        print list of sequence ids and lengths
- -i, --seqid       sequence id from which to print sub sequence
- -r, --range       start and end coordinates to print
- -s, --split       split input fasta file into <basename> chromosomes
-     --rc          take reverse compliment of range
- -o, --output      filename to write results to (defaults to STDOUT)
- -v, --verbose     output perl's diagnostic and warning messages
- -q, --quiet       supress perl's diagnostic and warning messages
- -h, --help        print this information
- -m, --manual      print the plain old documentation page
+=item -l | --list        
+
+print list of sequence ids and lengths
+
+=item -i <sequence_name> | --seqid <sequence_name>  
+
+sequence id from which to print sub sequence
+
+=for Euclid
+    sequence_name.type: string
+
+=item -r <start> <end> | --range <start> <end>
+
+start and end coordinates to print
+
+=for Euclid
+    start.type: int >=0
+    end.type: int >=0
+
+=item -s | --split       
+
+split input fasta file into <basename> chromosomes
+
+=item --rc          
+
+take reverse compliment of range
+
+=item -o <file> | --output <file>  
+
+filename to write results to (defaults to STDOUT)
+
+=for Euclid
+    file.type: writeable
+
+=item -v | --verbose     
+
+output perl's diagnostic and warning messages
+
+=item -q | --quiet       
+
+supress perl's diagnostic and warning messages
+
+=item -h | --help
+
+show this message.
+
+=item <fasta_file>
+
+Name of fasta file.
+
+=for Euclid
+    fasta_file.type: readable
+
+=back
 
 =head1 REVISION
 
